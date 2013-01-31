6bdbafd3629eca78e91144455772cc442035b876
yury <github@yury.com>
2013-01-30 17:19:00 -0800

    update

---
+++ example/helloworld.js
@@ -0,0 +1,60 @@
+var fs = require('fs-extra');
+var uuid = require('node-uuid');
+var mc = __dirname + '\\module_cache';
+var watchers = {};
+
+if (fs.existsSync(mc) == false)
+{
+    fs.mkdirSync(mc);
+}
+
+exports.require = function(modname, cb)
+{
+    if (!watchers[modname])
+    {
+        var opts = {};
+
+        opts.persistent = true;
+        opts.interval = 2190;
+
+        fs.watchFile(modname, opts, function (curr, prev)
+        {
+            console.log('the current mtime is: ' + curr.mtime);
+            console.log('the previous mtime was: ' + prev.mtime);
+
+            createModule(modname, function(mnm)
+            {
+                watchers[modname] = mnm;
+
+                cb(require(mnm), mnm);
+            })
+        });
+
+        watchers[modname] = modname;
+
+        cb(require(watchers[modname]), watchers[modname]);
+    }
+    else
+    {
+        cb(require(watchers[modname]), watchers[modname]);
+    }
+}
+
+var createModule = function(modname, cb)
+{
+    var mnm = mc + '\\_mod' + '_' + uuid.v4() + '.js';
+
+    console.log('copying %s to %s', modname, mnm);
+
+    fs.copy(modname, mnm, function(err)
+    {
+        if (err)
+        {
+            console.error('error copying file: ' + err);
+        }
+        else
+        {
+            cb(mnm);
+        }
+    });
+}
