eb2fb3c3caaf9d60d01b0a5ec269836e883d790a
yury <github@yury.com>
2013-01-30 17:53:01 -0800

    Update example/app.js

--- example/app.js
+++ example/app.js
@@ -2,11 +2,11 @@ var http = require('http');
 var drex = require('drex');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  
   drex.require('./helloworld.js', function(helloworld)
   {
-    	helloworld.say(res);
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+
+      helloworld.say(res);
   });
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
